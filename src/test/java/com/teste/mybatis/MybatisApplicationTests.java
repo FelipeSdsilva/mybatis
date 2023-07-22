96a46627559e89668c5da5892dd5c13895855eb7
Felipe Sousa <felipe.fps09@hotmail.com>
2023-07-22 09:40:31 -0300

    Resolve conflicts in .gitignore

---
+++ src/test/java/com/teste/mybatis/MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
